7955985856ddf513632478a8924920a004f9f45e
Gregory Boissinot <gregory.boissinot@gmail.com>
2011-12-30 17:00:56 +0100

    Add an initializer

---
+++ src/main/java/org/jenkinsci/plugins/envinject/AliasInitializer.java
@@ -0,0 +1,19 @@
+package org.jenkinsci.plugins.envinject;
+
+import hudson.init.InitMilestone;
+import hudson.init.Initializer;
+import hudson.model.Items;
+
+/**
+ * @author Gregory Boissinot
+ */
+public class AliasInitializer {
+
+    @Initializer(before = InitMilestone.PLUGINS_STARTED)
+    @SuppressWarnings("unused")
+    public static void addAliases() {
+        Items.XSTREAM.alias("EnvInjectJobProperty", EnvInjectJobProperty.class);
+        Items.XSTREAM.alias("EnvInjectBuildWrapper", EnvInjectBuildWrapper.class);
+        Items.XSTREAM.alias("EnvInjectBuilder", EnvInjectBuilder.class);
+    }
+}
