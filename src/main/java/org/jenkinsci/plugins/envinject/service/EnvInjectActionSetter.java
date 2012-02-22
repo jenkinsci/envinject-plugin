9d5c4664f182bdc893bf30fc25e10401bd56a38f
Gregory Boissinot <gregory.boissinot@gmail.com>
2012-02-22 23:48:54 +0100

    Add a check to avoid NullPointerException

--- src/main/java/org/jenkinsci/plugins/envinject/service/EnvInjectActionSetter.java
+++ src/main/java/org/jenkinsci/plugins/envinject/service/EnvInjectActionSetter.java
@@ -29,15 +29,17 @@ public class EnvInjectActionSetter implements Serializable {
         if (envInjectAction != null) {
             envInjectAction.overrideAll(envMap);
         } else {
-            envInjectAction = new EnvInjectPluginAction(build, rootPath.act(new Callable<Map<String, String>, EnvInjectException>() {
-                public Map<String, String> call() throws EnvInjectException {
-                    HashMap<String, String> result = new HashMap<String, String>();
-                    result.putAll(EnvVars.masterEnvVars);
-                    return result;
-                }
-            }));
-            envInjectAction.overrideAll(envMap);
-            build.addAction(envInjectAction);
+            if (rootPath != null) {
+                envInjectAction = new EnvInjectPluginAction(build, rootPath.act(new Callable<Map<String, String>, EnvInjectException>() {
+                    public Map<String, String> call() throws EnvInjectException {
+                        HashMap<String, String> result = new HashMap<String, String>();
+                        result.putAll(EnvVars.masterEnvVars);
+                        return result;
+                    }
+                }));
+                envInjectAction.overrideAll(envMap);
+                build.addAction(envInjectAction);
+            }
         }
     }
 }
