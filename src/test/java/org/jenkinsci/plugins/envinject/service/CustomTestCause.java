516384e6e708b0b3d929b83fedd7941bc69d8456
Gregory Boissinot <gregory.boissinot@gmail.com>
2012-08-05 01:57:32 +0200

    Initial import of build cause test

---
+++ src/test/java/org/jenkinsci/plugins/envinject/service/CustomTestCause.java
@@ -0,0 +1,14 @@
+package org.jenkinsci.plugins.envinject.service;
+
+import hudson.model.Cause;
+
+/**
+ * @author Gregory Boissinot
+ */
+public class CustomTestCause extends Cause {
+
+    @Override
+    public String getShortDescription() {
+        return "Short Custom Cause";
+    }
+}
