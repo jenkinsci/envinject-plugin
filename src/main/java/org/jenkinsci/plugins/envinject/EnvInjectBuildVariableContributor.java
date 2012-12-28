368ba74a6ed995e89013f860827a47920e9668c7
Gregory Boissinot <gregory.boissinot@gmail.com>
2012-12-28 20:01:14 +0100

    Fix JENKINS-16219

--- src/main/java/org/jenkinsci/plugins/envinject/EnvInjectBuildVariableContributor.java
+++ src/main/java/org/jenkinsci/plugins/envinject/EnvInjectBuildVariableContributor.java
@@ -3,20 +3,36 @@ package org.jenkinsci.plugins.envinject;
 import hudson.Extension;
 import hudson.model.AbstractBuild;
 import hudson.model.BuildVariableContributor;
+import hudson.model.ParameterValue;
+import hudson.model.ParametersAction;
 
 import java.util.Map;
 
 /**
  * @author Gregory Boissinot
  */
+
+/**
+ * Overriding job parameters with environment variables populated by EnvInject plugin
+ */
 @Extension
 public class EnvInjectBuildVariableContributor extends BuildVariableContributor {
 
     @Override
     public void buildVariablesFor(AbstractBuild build, Map<String, String> variables) {
-        EnvInjectPluginAction envInjectAction = build.getAction(EnvInjectPluginAction.class);
-        if (envInjectAction != null) {
-            variables.putAll(envInjectAction.getEnvMap());
+        ParametersAction parameters = build.getAction(ParametersAction.class);
+        //Only for a parameterized job
+        if (parameters != null) {
+            EnvInjectPluginAction envInjectAction = build.getAction(EnvInjectPluginAction.class);
+            if (envInjectAction != null) {
+                for (ParameterValue p : parameters) {
+                    String key = p.getName();
+                    Map<String, String> injectedEnvVars = envInjectAction.getEnvMap();
+                    if (injectedEnvVars.containsKey(key)) {
+                        variables.put(key, injectedEnvVars.get(key));
+                    }
+                }
+            }
         }
     }
 }
