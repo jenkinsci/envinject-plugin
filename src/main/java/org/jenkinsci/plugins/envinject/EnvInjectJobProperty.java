e92085799b2d04f34a689e31784b6c5292a60456
Nicolas De loof <nicolas.deloof@gmail.com>
2014-06-02 12:33:10 +0200

    [FIXED JENKINS-19852]

--- src/main/java/org/jenkinsci/plugins/envinject/EnvInjectJobProperty.java
+++ src/main/java/org/jenkinsci/plugins/envinject/EnvInjectJobProperty.java
@@ -135,7 +135,7 @@ public class EnvInjectJobProperty<T extends Job<?, ?>> extends JobProperty<T> {
             // Don't let non RUN_SCRIPT users set arbitrary groovy script
             property.info = new EnvInjectJobPropertyInfo(property.info.propertiesFilePath, property.info.propertiesContent,
                                                          property.info.getScriptFilePath(), property.info.getScriptContent(),
-                                                         property.info.getGroovyScriptContent(),
+                                                         this.info != null ? this.info.getGroovyScriptContent() : "",
                                                          property.info.isLoadFilesFromMaster());
         }
         return property;
