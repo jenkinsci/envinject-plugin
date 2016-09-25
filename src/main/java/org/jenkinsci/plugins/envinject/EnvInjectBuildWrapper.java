f01aa37c3cd7bbd950089d4c4e782967846f3ea1
Oleg Nenashev <o.v.nenashev@gmail.com>
2016-09-25 03:09:24 +0200

    FindBugs: Missing variable init in the default EnvInjectBuildWrapper constructor

--- src/main/java/org/jenkinsci/plugins/envinject/EnvInjectBuildWrapper.java
+++ src/main/java/org/jenkinsci/plugins/envinject/EnvInjectBuildWrapper.java
@@ -43,6 +43,7 @@ public class EnvInjectBuildWrapper extends BuildWrapper implements Serializable 
 
     @Deprecated
     public EnvInjectBuildWrapper() {
+        this.info = null;
     }
 
     /**
