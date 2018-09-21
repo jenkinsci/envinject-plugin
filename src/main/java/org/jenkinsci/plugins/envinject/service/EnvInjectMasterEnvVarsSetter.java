d7137f55ea835561c49d33f098625e61f0cec9fc
Jeff Pearce <jxpearce@godaddy.com>
2018-09-21 15:48:38 -0700

    Use safe reflection for Java 11

--- src/main/java/org/jenkinsci/plugins/envinject/service/EnvInjectMasterEnvVarsSetter.java
+++ src/main/java/org/jenkinsci/plugins/envinject/service/EnvInjectMasterEnvVarsSetter.java
@@ -3,12 +3,15 @@ package org.jenkinsci.plugins.envinject.service;
 import hudson.EnvVars;
 import hudson.Main;
 import hudson.Platform;
+import hudson.util.VersionNumber;
 import jenkins.security.MasterToSlaveCallable;
 import org.jenkinsci.lib.envinject.EnvInjectException;
 
 import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import javax.annotation.Nonnull;
+import jenkins.model.Jenkins;
 
 /**
  * @author Gregory Boissinot
@@ -16,6 +19,11 @@ import javax.annotation.Nonnull;
 public class EnvInjectMasterEnvVarsSetter extends MasterToSlaveCallable<Void, EnvInjectException> {
 
     private @Nonnull EnvVars enVars;
+    
+    // TODO: this may not be the version of core that gets the fix allowing us to
+    // use safe reflection to set the Platform. The exact version needs to be updated 
+    // after that version is released.
+    private static VersionNumber jenkinsVersionWithSetPlatform = new VersionNumber("2.139");
 
     public EnvInjectMasterEnvVarsSetter(@Nonnull EnvVars enVars) {
         this.enVars = enVars;
@@ -24,22 +32,23 @@ public class EnvInjectMasterEnvVarsSetter extends MasterToSlaveCallable<Void, En
     @Override
     public Void call() throws EnvInjectException {
         try {
-            Field platformField = EnvVars.class.getDeclaredField("platform");
-            platformField.setAccessible(true);
-            platformField.set(enVars, Platform.current());
+            if (Jenkins.getVersion().isOlderThan(jenkinsVersionWithSetPlatform)) {
+                Field platformField = EnvVars.class.getDeclaredField("platform");
+
+                platformField.setAccessible(true);
+                platformField.set(enVars, Platform.current());                
+            } else {
+                Method method = EnvVars.class.getDeclaredMethod("setPlatform", Platform.class);
+                method.invoke(enVars, Platform.current());
+            }
             if (Main.isUnitTest || Main.isDevelopmentMode) {
                 enVars.remove("MAVEN_OPTS");
             }
-            Field masterEnvVarsFiled = EnvVars.class.getDeclaredField("masterEnvVars");
-            masterEnvVarsFiled.setAccessible(true);
-            Field modifiersField = Field.class.getDeclaredField("modifiers");
-            modifiersField.setAccessible(true);
-            modifiersField.setInt(masterEnvVarsFiled, masterEnvVarsFiled.getModifiers() & ~Modifier.FINAL);
-            masterEnvVarsFiled.set(null, enVars);
-        } catch (IllegalAccessException iae) {
-            throw new EnvInjectException(iae);
-        } catch (NoSuchFieldException nsfe) {
-            throw new EnvInjectException(nsfe);
+            
+            EnvVars.masterEnvVars.clear();
+            EnvVars.masterEnvVars.putAll(enVars); 
+        } catch (IllegalAccessException | NoSuchFieldException | NoSuchMethodException | InvocationTargetException exception) {
+            throw new EnvInjectException(exception);
         }
 
         return null;
