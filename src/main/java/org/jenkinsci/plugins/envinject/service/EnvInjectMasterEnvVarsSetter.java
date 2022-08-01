9b9e962daaec8a544b84f00df23129576a9dea84
Konstantin Bulanov <3480411+bulanovk@users.noreply.github.com>
2022-08-01 12:21:34 +0400

    [JENKINS-68930] Fix Java17 Error on agent start (#206)
    * Fix JENKINS-68930. Use WA to work with modifiers filtered since JDK12.
    --add-opens java.base/java.lang=ALL-UNNAMED --add-opens java.base/java.lang.reflect=ALL-UNNAMED
    
    * Update src/main/java/org/jenkinsci/plugins/envinject/service/EnvInjectMasterEnvVarsSetter.java
    
    Co-authored-by: Alexander Brandes <brandes.alexander@web.de>
    
    * Throw NoSuchFieldException exception if there is no modifiers field
    
    Co-authored-by: Alexander Brandes <brandes.alexander@web.de>

--- src/main/java/org/jenkinsci/plugins/envinject/service/EnvInjectMasterEnvVarsSetter.java
+++ src/main/java/org/jenkinsci/plugins/envinject/service/EnvInjectMasterEnvVarsSetter.java
@@ -7,7 +7,10 @@ import jenkins.security.MasterToSlaveCallable;
 import org.jenkinsci.lib.envinject.EnvInjectException;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
+
 import edu.umd.cs.findbugs.annotations.NonNull;
 
 /**
@@ -21,6 +24,23 @@ public class EnvInjectMasterEnvVarsSetter extends MasterToSlaveCallable<Void, En
         this.enVars = enVars;
     }
 
+    private Field getModifiers() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException, NoSuchFieldException {
+        Method getDeclaredFields0 = Class.class.getDeclaredMethod("getDeclaredFields0", boolean.class);
+        getDeclaredFields0.setAccessible(true);
+        Field[] fields = (Field[]) getDeclaredFields0.invoke(Field.class, false);
+        Field modifiers = null;
+        for (Field each : fields) {
+            if ("modifiers".equals(each.getName())) {
+                modifiers = each;
+                break;
+            }
+        }
+        if (modifiers == null) {
+            throw new NoSuchFieldException();
+        }
+        return modifiers;
+    }
+
     @Override
     public Void call() throws EnvInjectException {
         try {
@@ -32,14 +52,14 @@ public class EnvInjectMasterEnvVarsSetter extends MasterToSlaveCallable<Void, En
             }
             Field masterEnvVarsFiled = EnvVars.class.getDeclaredField("masterEnvVars");
             masterEnvVarsFiled.setAccessible(true);
-            Field modifiersField = Field.class.getDeclaredField("modifiers");
+            Field modifiersField = getModifiers();
             modifiersField.setAccessible(true);
             modifiersField.setInt(masterEnvVarsFiled, masterEnvVarsFiled.getModifiers() & ~Modifier.FINAL);
             masterEnvVarsFiled.set(null, enVars);
-        } catch (IllegalAccessException iae) {
+        } catch (IllegalAccessException | NoSuchFieldException iae) {
             throw new EnvInjectException(iae);
-        } catch (NoSuchFieldException nsfe) {
-            throw new EnvInjectException(nsfe);
+        } catch (InvocationTargetException | NoSuchMethodException e) {
+            throw new RuntimeException(e);
         }
 
         return null;
