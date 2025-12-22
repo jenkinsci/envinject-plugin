8c4f78cc11e3c4ab02626f5362d6dfd6e9dcbc6c
Abhijeet Ghosh <abhijeet.ghosh@zendesk.com>
2025-12-22 12:11:56 +0530

    Added tests for env var setter.

---
+++ src/test/java/org/jenkinsci/plugins/envinject/EnvInjectMasterEnvVarsSetterTest.java
@@ -0,0 +1,282 @@
+package org.jenkinsci.plugins.envinject.service;
+
+import hudson.EnvVars;
+import hudson.Main;
+import org.jenkinsci.lib.envinject.EnvInjectException;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+
+import static org.junit.Assert.*;
+
+public class EnvInjectMasterEnvVarsSetterTest {
+
+    private EnvVars originalMasterEnvVars;
+    private boolean originalIsUnitTest;
+    private boolean originalIsDevelopmentMode;
+
+    @Before
+    public void setUp() throws Exception {
+        // Backup original state
+        originalMasterEnvVars = new EnvVars(EnvVars.masterEnvVars);
+        EnvVars.masterEnvVars.clear();
+        
+        // Backup original Main flags
+        originalIsUnitTest = Main.isUnitTest;
+        originalIsDevelopmentMode = Main.isDevelopmentMode;
+        
+        // Set default test state
+        setMainFlags(false, false);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        // Restore original state
+        EnvVars.masterEnvVars.clear();
+        EnvVars.masterEnvVars.putAll(originalMasterEnvVars);
+        
+        // Restore original Main flags
+        setMainFlags(originalIsUnitTest, originalIsDevelopmentMode);
+    }
+    
+    private void setMainFlags(boolean isUnitTest, boolean isDevelopmentMode) throws Exception {
+        try {
+            Field isUnitTestField = Main.class.getDeclaredField("isUnitTest");
+            isUnitTestField.setAccessible(true);
+            
+            // Remove final modifier if present
+            Field modifiersField = Field.class.getDeclaredField("modifiers");
+            modifiersField.setAccessible(true);
+            modifiersField.setInt(isUnitTestField, isUnitTestField.getModifiers() & ~Modifier.FINAL);
+            
+            isUnitTestField.setBoolean(null, isUnitTest);
+        } catch (Exception e) {
+            // If we can't modify the field, use System property as fallback
+            System.setProperty("jenkins.test.isUnitTest", String.valueOf(isUnitTest));
+        }
+        
+        try {
+            Field isDevelopmentModeField = Main.class.getDeclaredField("isDevelopmentMode");
+            isDevelopmentModeField.setAccessible(true);
+            
+            Field modifiersField = Field.class.getDeclaredField("modifiers");
+            modifiersField.setAccessible(true);
+            modifiersField.setInt(isDevelopmentModeField, isDevelopmentModeField.getModifiers() & ~Modifier.FINAL);
+            
+            isDevelopmentModeField.setBoolean(null, isDevelopmentMode);
+        } catch (Exception e) {
+            // If we can't modify the field, use System property as fallback
+            System.setProperty("jenkins.test.isDevelopmentMode", String.valueOf(isDevelopmentMode));
+        }
+    }
+
+    @Test
+    public void testCall_WhenEnvVarsEqual_ReturnsNull() throws Exception {
+        // Given
+        EnvVars testEnvVars = new EnvVars(EnvVars.masterEnvVars);
+        EnvInjectMasterEnvVarsSetter setter = new EnvInjectMasterEnvVarsSetter(testEnvVars);
+
+        // When
+        Void result = setter.call();
+
+        // Then
+        assertNull(result);
+    }
+
+    @Test
+    public void testCall_WhenSameKeys_UpdatesValues() throws Exception {
+        // Given
+        EnvVars.masterEnvVars.put("KEY1", "oldValue");
+
+        EnvVars newEnvVars = new EnvVars();
+        newEnvVars.put("KEY1", "newValue");
+
+        EnvInjectMasterEnvVarsSetter setter = new EnvInjectMasterEnvVarsSetter(newEnvVars);
+
+        // When
+        Void result = setter.call();
+
+        // Then
+        assertNull(result);
+        assertEquals("newValue", EnvVars.masterEnvVars.get("KEY1"));
+    }
+
+    @Test
+    public void testCall_WhenDifferentKeys_ReplacesVars() throws Exception {
+        // Given
+        EnvVars.masterEnvVars.put("OLD_KEY", "oldValue");
+
+        EnvVars newEnvVars = new EnvVars();
+        newEnvVars.put("NEW_KEY", "newValue");
+
+        EnvInjectMasterEnvVarsSetter setter = new EnvInjectMasterEnvVarsSetter(newEnvVars);
+
+        // When
+        Void result = setter.call();
+
+        // Then
+        assertNull(result);
+        assertTrue(EnvVars.masterEnvVars.containsKey("NEW_KEY"));
+        assertEquals("newValue", EnvVars.masterEnvVars.get("NEW_KEY"));
+    }
+
+    @Test
+    public void testCall_InUnitTestMode_RemovesMavenOpts() throws Exception {
+        // Given
+        try {
+            setMainFlags(true, false);
+        } catch (Exception e) {
+            // If we can't set the flags, skip this test
+            org.junit.Assume.assumeNoException("Cannot modify Main flags in this environment", e);
+        }
+
+        EnvVars newEnvVars = new EnvVars();
+        newEnvVars.put("MAVEN_OPTS", "should-be-removed");
+        newEnvVars.put("KEEP_ME", "should-remain");
+
+        EnvInjectMasterEnvVarsSetter setter = new EnvInjectMasterEnvVarsSetter(newEnvVars);
+
+        // When
+        setter.call();
+
+        // Then - Only test if we successfully set unit test mode
+        if (Main.isUnitTest) {
+            assertFalse(EnvVars.masterEnvVars.containsKey("MAVEN_OPTS"));
+        }
+        assertTrue(EnvVars.masterEnvVars.containsKey("KEEP_ME"));
+    }
+
+    @Test
+    public void testCall_InDevelopmentMode_RemovesMavenOpts() throws Exception {
+        // Given
+        try {
+            setMainFlags(false, true);
+        } catch (Exception e) {
+            // If we can't set the flags, skip this test
+            org.junit.Assume.assumeNoException("Cannot modify Main flags in this environment", e);
+        }
+
+        EnvVars newEnvVars = new EnvVars();
+        newEnvVars.put("MAVEN_OPTS", "should-be-removed");
+        newEnvVars.put("KEEP_ME", "should-remain");
+
+        EnvInjectMasterEnvVarsSetter setter = new EnvInjectMasterEnvVarsSetter(newEnvVars);
+
+        // When
+        setter.call();
+
+        // Then - Only test if we successfully set development mode
+        if (Main.isDevelopmentMode) {
+            assertFalse(EnvVars.masterEnvVars.containsKey("MAVEN_OPTS"));
+        }
+        assertTrue(EnvVars.masterEnvVars.containsKey("KEEP_ME"));
+    }
+
+    @Test
+    public void testCall_WithMultipleVars_UpdatesCorrectly() throws Exception {
+        // Given
+        EnvVars.masterEnvVars.put("EXISTING", "existing");
+
+        EnvVars newEnvVars = new EnvVars();
+        newEnvVars.put("NEW_VAR", "newValue");
+        newEnvVars.put("ANOTHER_VAR", "anotherValue");
+
+        EnvInjectMasterEnvVarsSetter setter = new EnvInjectMasterEnvVarsSetter(newEnvVars);
+
+        // When
+        setter.call();
+
+        // Then
+        assertTrue(EnvVars.masterEnvVars.containsKey("NEW_VAR"));
+        assertTrue(EnvVars.masterEnvVars.containsKey("ANOTHER_VAR"));
+        assertEquals("newValue", EnvVars.masterEnvVars.get("NEW_VAR"));
+        assertEquals("anotherValue", EnvVars.masterEnvVars.get("ANOTHER_VAR"));
+    }
+
+    @Test
+    public void testCall_HandlesReflectionGracefully() throws Exception {
+        // Given - This test exercises the reflection code paths
+        EnvVars.masterEnvVars.clear();
+
+        EnvVars newEnvVars = new EnvVars();
+        newEnvVars.put("TEST_VAR", "testValue");
+        newEnvVars.put("PATH", "/usr/bin");
+
+        EnvInjectMasterEnvVarsSetter setter = new EnvInjectMasterEnvVarsSetter(newEnvVars);
+
+        // When - This will exercise the reflection code in call()
+        Void result = setter.call();
+
+        // Then - Should succeed regardless of Java version
+        assertNull(result);
+        assertEquals("testValue", EnvVars.masterEnvVars.get("TEST_VAR"));
+        assertEquals("/usr/bin", EnvVars.masterEnvVars.get("PATH"));
+    }
+
+    @Test
+    public void testCall_WithEmptyNewVars_ClearsExisting() throws Exception {
+        // Given
+        EnvVars.masterEnvVars.put("TO_BE_CLEARED", "value");
+
+        EnvVars newEnvVars = new EnvVars();
+        // Empty EnvVars should trigger the different keys path
+
+        EnvInjectMasterEnvVarsSetter setter = new EnvInjectMasterEnvVarsSetter(newEnvVars);
+
+        // When
+        setter.call();
+
+        // Then - After processing, the old key should be gone
+        assertFalse(EnvVars.masterEnvVars.containsKey("TO_BE_CLEARED"));
+    }
+
+    @Test
+    public void testConstructor() {
+        // Given
+        EnvVars testVars = new EnvVars();
+        testVars.put("TEST", "value");
+
+        // When
+        EnvInjectMasterEnvVarsSetter setter = new EnvInjectMasterEnvVarsSetter(testVars);
+
+        // Then
+        assertNotNull(setter);
+    }
+
+    @Test
+    public void testCall_WithBothMavenOptsAndOtherVars() throws Exception {
+        // Given - Only test MAVEN_OPTS removal if we can modify flags
+        boolean canSetFlags = false;
+        try {
+            setMainFlags(true, false);
+            canSetFlags = Main.isUnitTest;
+        } catch (Exception e) {
+            // Cannot modify flags, test will focus on other behavior
+        }
+
+        EnvVars newEnvVars = new EnvVars();
+        newEnvVars.put("MAVEN_OPTS", "should-be-removed");
+        newEnvVars.put("JAVA_HOME", "/usr/lib/jvm/java");
+        newEnvVars.put("USER", "testuser");
+
+        EnvInjectMasterEnvVarsSetter setter = new EnvInjectMasterEnvVarsSetter(newEnvVars);
+
+        // When
+        setter.call();
+
+        // Then
+        if (canSetFlags) {
+            assertFalse("MAVEN_OPTS should be removed in unit test mode", 
+                       EnvVars.masterEnvVars.containsKey("MAVEN_OPTS"));
+        }
+        assertTrue("JAVA_HOME should remain", 
+                  EnvVars.masterEnvVars.containsKey("JAVA_HOME"));
+        assertTrue("USER should remain", 
+                  EnvVars.masterEnvVars.containsKey("USER"));
+        assertEquals("/usr/lib/jvm/java", EnvVars.masterEnvVars.get("JAVA_HOME"));
+        assertEquals("testuser", EnvVars.masterEnvVars.get("USER"));
+    }
+}
